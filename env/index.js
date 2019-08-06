d74bf12e26235bd8b41570e91ff31588de966632
Robb Traister <ltraister@virtru.com>
2019-08-06 09:09:27 -0400

    add config default

--- env/index.js
+++ env/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = ({ react }) => ({
+module.exports = ({ react = {} }) => ({
   react: {
     appId: process.env.APP_ID || react.appId || 'app',
     fileLimit: Number(process.env.FILE_LIMIT) || react.fileLimit || 100 * 1024
